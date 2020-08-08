d11a6e542d69469ca512a2a962282ff8745b46c5
Milan Panic <panicmilan91@gmail.com>
2020-08-08 18:34:28 +0200

    Fix

--- datasets/kitti.py
+++ datasets/kitti.py
@@ -47,8 +47,8 @@ class KITTIDataset(Dataset):
         raw_odometry_matrix_2 = current_dataset.poses[index_in_dataset + 1][:3, :4]
 
         # Rotation angles for x and z are replaced compared to referent code
-        preprocessed_ground_truth_1 = self.preprocess_odometry_matrix(raw_odometry_matrix_1)
-        preprocessed_ground_truth_2 = self.preprocess_odometry_matrix(raw_odometry_matrix_2)
+        preprocessed_ground_truth_1 = self.preprocess_odometry_matrix(raw_odometry_matrix_1, current_dataset.poses[0][:3, :4])
+        preprocessed_ground_truth_2 = self.preprocess_odometry_matrix(raw_odometry_matrix_2, current_dataset.poses[0][:3, :4])
 
         # Return two consecutive images
         return (
@@ -70,7 +70,7 @@ class KITTIDataset(Dataset):
         return image
 
     @staticmethod
-    def preprocess_odometry_matrix(odometry_matrix: np.ndarray) -> torch.Tensor:
+    def preprocess_odometry_matrix(odometry_matrix: np.ndarray, initial_rotation_matrix) -> torch.Tensor:
         """
         Rotation matrix to Euler angles
         Return [torch.Tensor]: Angles and translation
@@ -78,7 +78,11 @@ class KITTIDataset(Dataset):
         """
         rotation = Rotation.from_matrix(odometry_matrix[0:3, 0:3])
         translation = odometry_matrix[:, 3]
+
+        # TODO: To be checked
+        translation = initial_rotation_matrix[:3,:3].dot(translation)
         # x and z are replaced compared to original
         rotation_angles = rotation.as_euler('zxy', degrees=False)
+
         preprocessed_output = np.concatenate((rotation_angles.flatten(), translation.flatten()))
         return torch.from_numpy(preprocessed_output)
