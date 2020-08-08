ee86a3477719dcf72affcf19fe58a79469ff6eb3
Milan Panic <panicmilan91@gmail.com>
2020-08-08 16:58:09 +0200

    Minor changes

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ def main():
     parser.add_argument('--batch-size', type=int,
                         default=10)
     parser.add_argument('--validation-size', type=int,
-                        default=1000)
+                        default=10)
     parser.add_argument('--epochs', type=int,
                         default=40)
     parser.add_argument('--tboard-step', type=int,
-                        default=10)
+                        default=50)
     parser.add_argument('--save-model-step', type=int,
                         default=200)
     parser.add_argument('--learning-rate', type=float,
-                        default=0.001)
+                        default=0.0005) #0.0005
     parser.add_argument('--pretrained-flownet', type=str,
                         default="")
     parser.add_argument("--model-tag", type=str, default="initial_model")
@@ -40,15 +40,15 @@ def main():
     summary_writer = SummaryWriter()
 
     # Train set
-    dataset = KITTIDataset(args.kitti_base_dir, [0, 2, 5, 6, 7, 8, 9, 10])
+    dataset = KITTIDataset(args.kitti_base_dir, [0]) #, 2, 5, 6, 7, 8, 9, 10
     dataloader = DataLoader(
         dataset=dataset,
         batch_size=args.batch_size,
-        shuffle=False,
+        shuffle=True,
         drop_last=True
     )
     # Validation set:
-    validation_dataset = KITTIDataset(args.kitti_base_dir, [1, 3, 4])
+    validation_dataset = KITTIDataset(args.kitti_base_dir, [0]) #, 3, 4
     validation_dataloader = DataLoader(
         dataset=validation_dataset,
         batch_size=1,
@@ -56,6 +56,8 @@ def main():
         drop_last=True
     )
     model = FlowNetS()
+    # from models.FlownetSimpleLikeV2 import FlowNetS_V2
+    # model = FlowNetS_V2()
 
     if args.pretrained_flownet:
         pretrained_w = torch.load(args.pretrained_flownet, map_location='cpu')
@@ -77,15 +79,17 @@ def main():
     learning_rate = args.learning_rate
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     loss = RMSEWeightedLoss()
+    #lr_sheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
 
-    global_step = 0
+    global_step = 1000
 
     for epoch in range(args.epochs):
-        print("epoch {}".format(epoch))
+        print("Epoch:{}".format(epoch))
         epoch_steps = 0
         total_loss = 0
         raw_output_diff = torch.zeros(6)
         for batch in dataloader:
+            model.train()
             cam0_img, cam1_img, ground_truth = batch
 
             input_tensor = torch.cat((cam0_img, cam1_img), 1)
@@ -97,7 +101,7 @@ def main():
             batch_loss = loss(y, ground_truth)
             total_loss += batch_loss
             raw_output_diff += torch.mean(torch.abs(y - ground_truth), dim=0)
-            if global_step > 0 and global_step % args.tboard_step == 0:
+            if global_step > 0 and epoch_steps > 0 and global_step % args.tboard_step == 0:
                 summary_writer.add_scalar("train_loss/Batch loss", batch_loss, global_step)
                 print("Step {} avg epoch loss: {}".format(global_step, total_loss / epoch_steps))
                 print("Step {} avg raw diff: {}".format(global_step, raw_output_diff / epoch_steps))
@@ -105,14 +109,13 @@ def main():
                 for i in range(6):
                     if i < 3:
                         summary_writer.add_scalar("train_raw/Average Train RotAngle{} degrees diff".format(i),
-                                                  raw_output_diff[i] * 2 * np.pi / epoch_steps, global_step)
+                                                  raw_output_diff[i] / np.pi*180 / epoch_steps, global_step)
                     else:
                         summary_writer.add_scalar("train_raw/Average Train TransVector{} diff".format(i - 3),
                                                   raw_output_diff[i] / epoch_steps,
                                                   global_step)
                     summary_writer.add_scalar("train_loss/Epoch average loss", total_loss / epoch_steps, global_step)
-                do_validation(validation_dataloader, model, args.validation_size, loss, use_cuda, global_step,
-                              summary_writer)
+                #val_loss = do_validation(validation_dataloader, model, args.validation_size, loss, use_cuda, global_step, summary_writer)
             # zero the parameter gradients
             optimizer.zero_grad()
             batch_loss.backward()
@@ -135,6 +138,7 @@ def main():
 
 
 def do_validation(validation_dataloader, model, validation_size, loss, use_cuda, global_step, summary_writer):
+    model.eval()
     with torch.no_grad():
         validation_step = 0
         validation_loss = 0
@@ -159,7 +163,7 @@ def do_validation(validation_dataloader, model, validation_size, loss, use_cuda,
         for i in range(6):
             if i < 3:
                 summary_writer.add_scalar("val_raw/Average Val RotAngle{} degrees diff".format(i),
-                                          raw_output_diff[i] * 2 * np.pi / validation_step, global_step)
+                                          raw_output_diff[i] / np.pi*180 / validation_step, global_step)
             else:
                 summary_writer.add_scalar("val_raw/Average Val TransVector{} diff".format(i - 3),
                                           raw_output_diff[i] / validation_step,
@@ -167,7 +171,7 @@ def do_validation(validation_dataloader, model, validation_size, loss, use_cuda,
         summary_writer.add_scalar("val_loss/Val average loss", validation_loss / validation_step, global_step)
         print("Global step {} Validation avg loss: {}".format(global_step, validation_loss / validation_step))
         # TODO: Add random validation images
-
+        return validation_loss/validation_step
 
 if __name__ == '__main__':
     main()
--- models/FlownetSimpleLikeV2.py
+++ models/FlownetSimpleLikeV2.py
@@ -2,20 +2,6 @@ import torch
 import torch.nn as nn
 
 
-class RMSEWeightedLoss(nn.Module):
-    def __init__(self, eps=1e-6, beta=1):
-        super().__init__()
-        self.mse = nn.MSELoss()
-        self.eps = eps
-        self.beta = beta
-
-    def forward(self, yhat, y):
-        # Weighted RMSE loss
-        loss = self.beta*torch.sqrt(self.mse(y[:, :3], yhat[:, :3]) + self.eps)+torch.sqrt(self.mse(y[:, 3:], yhat[:, 3:]) + self.eps)
-
-        #loss = (self.beta**2) * self.mse(y[:, :3], yhat[:, :3]) + self.mse(y[:, 3:], yhat[:, 3:])
-        return loss
-
 # Taken and adapted from:
 # https://github.com/ClementPinard/FlowNetPytorch/blob/master/models/FlowNetS.py
 
@@ -35,10 +21,10 @@ def conv(batchNorm, in_planes, out_planes, kernel_size=3, stride=1):
         )
 
 
-class FlowNetS(nn.Module):
+class FlowNetS_V2(nn.Module):
 
     def __init__(self, batchNorm=True):
-        super(FlowNetS, self).__init__()
+        super(FlowNetS_V2, self).__init__()
 
         self.batchNorm = batchNorm
         self.conv1 = conv(self.batchNorm, 6, 64, kernel_size=7, stride=2)
@@ -52,13 +38,19 @@ class FlowNetS(nn.Module):
         self.conv6 = conv(self.batchNorm, 512, 1024, stride=2)
         self.conv6_1 = conv(self.batchNorm, 1024, 1024)
 
-        # TODO: Add regression for output
         # Dummy - rescale input, use RGB, this is just for testing
-        self.fc1 = nn.Linear(1024 * 3 * 10, 1000)
-        self.relu_fc1 = nn.ReLU()
-        self.fc2 = nn.Linear(1000, 100)
-        self.relu_fc2 = nn.ReLU()
-        self.output = nn.Linear(100, 6)
+        self.fc1_branch_angle = nn.Linear(1024 * 3 * 10, 500)
+        self.relu_fc1_branch_angle = nn.ReLU()
+        self.fc2_branch_angle = nn.Linear(500, 100)
+        self.relu_fc2_branch_angle = nn.ReLU()
+        self.output_branch_angle = nn.Linear(100, 3)
+
+        self.fc1_branch_translation = nn.Linear(1024 * 3 * 10, 500)
+        self.relu_fc1_branch_translation = nn.ReLU()
+        self.fc2_branch_translation = nn.Linear(500, 100)
+        self.relu_fc2_branch_translation = nn.ReLU()
+
+        self.output_branch_translation = nn.Linear(100, 3)
 
     def forward(self, x):
         # x is stacked 2 images - t, t+1
@@ -69,7 +61,13 @@ class FlowNetS(nn.Module):
         out_conv5 = self.conv5_1(self.conv5(out_conv4))
         out_conv6 = self.conv6_1(self.conv6(out_conv5))
 
-        fc1 = self.relu_fc1(self.fc1(out_conv6.reshape(-1, 1024*3*10)))
-        fc2 = self.relu_fc2(self.fc2(fc1))
+        feature_extraction_output = out_conv6.reshape(-1, 1024*3*10)
+        fc1_branch_angle = self.relu_fc1_branch_angle(self.fc1_branch_angle(feature_extraction_output))
+        fc2_branch_angle = self.relu_fc2_branch_angle(self.fc2_branch_angle(fc1_branch_angle))
+        output_branch_angle = self.output_branch_angle(fc2_branch_angle)
 
-        return self.output(fc2)
+        fc1_branch_translation = self.relu_fc1_branch_translation(self.fc1_branch_translation(feature_extraction_output))
+        fc2_branch_translation = self.relu_fc2_branch_translation(self.fc2_branch_translation(fc1_branch_translation))
+        output__branch_translation = self.output_branch_translation(fc2_branch_translation)
+
+        return torch.cat((output_branch_angle, output__branch_translation), dim=1)
