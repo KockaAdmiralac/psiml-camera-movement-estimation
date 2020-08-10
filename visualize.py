793e9cbd2be4437ee62b173dc89404ad8e8981ed
Luka Simić <git@kocka.tech>
2020-08-10 03:38:22 +0200

    Visualization changes.

--- visualize.py
+++ visualize.py
@@ -33,7 +33,7 @@ def get_model(use_cuda: bool, model_path: str) -> FlowNetS_V2:
 
 def write_poses(poses_file: TextIOWrapper, points: np.ndarray, rotations: np.ndarray,
                 use_cuda: bool, model: FlowNetS_V2, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]):
-    cam0_img, cam1_img, _, _ = batch
+    cam0_img, cam1_img, ground_truth, _ = batch
     input_tensor = torch.cat((cam0_img, cam1_img), 1)
     if use_cuda:
         input_tensor = input_tensor.cuda()
@@ -41,21 +41,27 @@ def write_poses(poses_file: TextIOWrapper, points: np.ndarray, rotations: np.nda
     y = y.to('cpu')
     rotation = sum(y[:, 0:3]).numpy()
     translation = sum(y[:, 3:6]).numpy()
-    r = Rotation.from_euler('zxy', rotations, degrees=False)
-    points += r.apply(translation)
+    #rotation = sum(ground_truth[:, 0:3]).numpy()
+    #translation = sum(ground_truth[:, 3:6]).numpy()
+    points += translation
     rotations += rotation
-    rotation_matrix = Rotation.from_euler('zxy', rotations, degrees=False).as_matrix().reshape(9)
-    concatenated_list = list(np.concatenate((rotation_matrix, points)))
-    poses_file.write(f"{' '.join([str(a) for a in concatenated_list])}\n")
+    rotation_matrix = Rotation.from_euler('zxy', rotations, degrees=False).as_matrix()
+    concatenated_list = [
+        rotation_matrix[0, 0], rotation_matrix[0, 1], rotation_matrix[0, 2], points[0],
+        rotation_matrix[1, 0], rotation_matrix[1, 1], rotation_matrix[1, 2], points[1],
+        rotation_matrix[2, 0], rotation_matrix[2, 1], rotation_matrix[2, 2], points[2]
+    ]
+    poses_file.write(f"{' '.join(['{:e}'.format(a) for a in concatenated_list])}\n")
     poses_file.flush()
 
 def main():
     args = get_args()
     use_cuda = torch.cuda.is_available()
     model = get_model(use_cuda, args.model)
-    points = np.array([0., 0., 0.])
-    rotations = np.array([0., 0., 0.])
     dataset = KITTIDataset(args.kitti_base_dir, [args.sequence])
+    first_pose = dataset.preprocess_odometry_matrix(dataset._datasets[0].poses[0])
+    points = first_pose[3:6].numpy()
+    rotations = first_pose[0:3].numpy()
     dataloader = DataLoader(
         dataset=dataset,
         batch_size=args.batch_size,
@@ -63,6 +69,13 @@ def main():
         drop_last=True
     )
     with open(os.path.join(args.poses_path, 'poses.txt'), 'w') as poses_file:
+        rotation_matrix = Rotation.from_euler('zxy', rotations, degrees=False).as_matrix()
+        concatenated_list = [
+            rotation_matrix[0, 0], rotation_matrix[0, 1], rotation_matrix[0, 2], points[0],
+            rotation_matrix[1, 0], rotation_matrix[1, 1], rotation_matrix[1, 2], points[1],
+            rotation_matrix[2, 0], rotation_matrix[2, 1], rotation_matrix[2, 2], points[2]
+        ]
+        poses_file.write(f"{' '.join(['{:e}'.format(a) for a in concatenated_list])}\n")
         with torch.no_grad():
             for i, batch in enumerate(dataloader):
                 write_poses(poses_file, points, rotations, use_cuda, model, batch)
